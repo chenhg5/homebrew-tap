f42eb664a23268ad84827b9ab31eb702ee045f7c
chaos <chg80333@163.com>
2018-07-11 17:58:28 -0700

    add mins.rb
    add mins.rb

---
+++ mins.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Mins < Formula
+  desc "a mini restful resource server for frontend workers. Make it easy!"
+  homepage ""
+  url "https://github.com/chenhg5/mins/releases/download/0.0.4/mins.tar.gz"
+  sha256 "b6f459a4bfdca841bf5c8e0ed5e797a4b1e1d23329a776335aafbb5c5eb0c174"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "cmake", ".", *std_cmake_args
+    bin.install "mins"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test mins`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
